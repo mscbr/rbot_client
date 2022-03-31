a0ce358309d14bd37518bcb8ca1d0cc576fa8d2d
mscbr <scibor.maciej@gmail.com>
2022-03-31 21:15:36 +0200

    fix: displaying all %

--- src/utils/converters.js
+++ src/utils/converters.js
@@ -1,6 +1,5 @@
 
 export const profitToProcent = (profitFloat) => {
-  if (profitFloat <= 1) return "0%";
   const arr = profitFloat.toFixed(3).toString().split('.')[1].split('');
   return arr[0] + arr[1] + '.' + arr[2] + '%';
 };
